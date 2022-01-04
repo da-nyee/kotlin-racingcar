06484b46ee0855b166856fa7a08021372f58a325
da-nyee <leede0418@gmail.com>
2022-01-04 18:40:22 +0900

    chore: 프로젝트 기본 세팅

---
+++ src/test/kotlin/com/example/kotlinracingcar/KotlinRacingcarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinracingcar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRacingcarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
