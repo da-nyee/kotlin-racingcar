7a842b521291e6984fcd5f35e984d9455c18da9f
da-nyee <leede0418@gmail.com>
2022-01-07 22:35:45 +0900

    chore: JUnit, AssertJ 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.21.0")
 }
 
 tasks.withType<KotlinCompile> {
