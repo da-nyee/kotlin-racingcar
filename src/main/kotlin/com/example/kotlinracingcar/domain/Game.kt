648b6497bd9b54763bb6ca1672135c499ba05f47
da-nyee <leede0418@gmail.com>
2022-01-10 01:34:06 +0900

    fix: 자동차를 전진 또는 멈추는 기능 버그 수정

--- src/main/kotlin/com/example/kotlinracingcar/domain/Game.kt
+++ src/main/kotlin/com/example/kotlinracingcar/domain/Game.kt
@@ -3,14 +3,16 @@ package com.example.kotlinracingcar.domain
 class Game(private val cars: Cars, val times: Times) {
 
     fun race(moveStrategy: MoveStrategy): Pair<List<Cars>, List<Car>> {
-        val allMovedCars = mutableListOf<Cars>()
+        val movedCars = mutableListOf(cars)
 
         repeat(times.times) {
-            val movedCars = cars.move(moveStrategy)
-            allMovedCars.add(movedCars)
+            val beforeMovedCars = movedCars.last()
+            val afterMovedCars = beforeMovedCars.move(moveStrategy)
+            movedCars.add(afterMovedCars)
         }
+        movedCars.removeAt(0)
 
-        return allMovedCars to findWinners(allMovedCars.last())
+        return movedCars to findWinners(movedCars.last())
     }
 
     private fun findWinners(movedCars: Cars): List<Car> {
