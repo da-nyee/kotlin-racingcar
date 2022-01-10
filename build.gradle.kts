6a18961b25cc5227ffde24734542e91d3e0bd29a
Daeun Lee <leede0418@likelion.org>
2022-01-11 01:55:12 +0900

    chore: 불필요한 의존성 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
 }
 
 group = "com.example"
@@ -16,11 +13,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("org.assertj:assertj-core:3.21.0")
 }
